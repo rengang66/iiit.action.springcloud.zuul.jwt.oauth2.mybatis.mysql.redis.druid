d75d9b748e94a61b99219585bd7a88d612da7eed
rengang66 <rengang66@sina.com>
2019-04-26 15:39:38 +0800

    V1.0 commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# springcloud_oauth2.0
+一个基于Spring Cloud + OAuth2.0 + Spring Security + Redis 的统一认证脚手架
+
+this application is about springcloud,zuul,jwt,oauth2,mybatis,mysql,redis and druid.
+
+通过zuul实现jwt.oauth2，ORM是mybatis，数据库采用mysql，缓存服务是redis.数据源druid
+
---
+++ eshop-server/src/main/java/com/iiit/action/springcloud/eshop/server/ServerCenterApplication.java
@@ -0,0 +1,14 @@
+package com.iiit.action.springcloud.eshop.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerCenterApplication.class, args);
+    }
+}
